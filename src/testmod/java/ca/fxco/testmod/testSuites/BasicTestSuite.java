0bad193ace29956f8db1160b88ace0d639efcc72
fx <28154542+fxmorin@users.noreply.github.com>
2023-05-27 16:28:34 -0400

    Use `@GameTestLib` annotation in BasicTestSuite

--- src/testmod/java/ca/fxco/testmod/testSuites/BasicTestSuite.java
+++ src/testmod/java/ca/fxco/testmod/testSuites/BasicTestSuite.java
@@ -1,14 +1,13 @@
 package ca.fxco.testmod.testSuites;
 
-import ca.fxco.gametestlib.gametest.GameTestUtil;
+import ca.fxco.api.gametestlib.gametest.GameTestLib;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 
 public class BasicTestSuite {
 
     // Make sure obsidian is still immovable
+    @GameTestLib
     @GameTest(timeoutTicks = 4)
-    public void immovable(GameTestHelper helper) {
-        GameTestUtil.initializeGameTestLib(helper);
-    }
+    public void immovable(GameTestHelper helper) {}
 }
