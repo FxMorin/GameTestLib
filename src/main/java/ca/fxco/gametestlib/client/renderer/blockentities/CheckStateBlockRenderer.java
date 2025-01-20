8ec70d5c5ee2e018d609903712587d701f7a543a
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-01-19 20:11:05 -0500

    Get completely rid of `RenderUtil` - The built-in debug renderers are much better. - Fixes translucency issues with the old renderer

--- src/main/java/ca/fxco/gametestlib/client/renderer/blockentities/CheckStateBlockRenderer.java
+++ src/main/java/ca/fxco/gametestlib/client/renderer/blockentities/CheckStateBlockRenderer.java
@@ -1,20 +1,18 @@
 package ca.fxco.gametestlib.client.renderer.blockentities;
 
-import ca.fxco.gametestlib.Utils.RenderUtils;
 import ca.fxco.gametestlib.blocks.CheckStateBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.debug.DebugRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.ARGB;
 import net.minecraft.world.level.block.state.BlockState;
-import org.joml.Matrix4f;
 
 @Environment(EnvType.CLIENT)
 public class CheckStateBlockRenderer<T extends CheckStateBlockEntity> implements BlockEntityRenderer<T> {
@@ -35,13 +33,13 @@ public class CheckStateBlockRenderer<T extends CheckStateBlockEntity> implements
             poseStack.translate(direction.getStepX(),direction.getStepY(),direction.getStepZ());
             poseStack.translate(-0.005F,-0.005F,-0.005F);
             poseStack.scale(1.01F, 1.01F, 1.01F);
-            Matrix4f matrix4f = poseStack.last().pose();
             BlockPos sidePos = blockEntity.getBlockPos().relative(direction);
             BlockState state = blockEntity.getLevel().getBlockState(blockEntity.getBlockPos().relative(direction));
             int color = blockEntity.getBlockStateExp().matches(state) ?
                     (blockEntity.isFailOnFound() ? RED : GREEN) :
                     GRAY;
-            RenderUtils.renderCube(matrix4f, multiBufferSource.getBuffer(RenderType.lightning()), color);
+            DebugRenderer.renderFilledUnitCube(poseStack, multiBufferSource, sidePos,
+                    ARGB.alpha(color), ARGB.red(color), ARGB.green(color), ARGB.blue(color));
             poseStack.popPose();
             int tick = blockEntity.getTick();
             if (tick != -1) {
--- src/main/java/ca/fxco/gametestlib/client/renderer/blockentities/EntityInsideBlockRenderer.java
+++ src/main/java/ca/fxco/gametestlib/client/renderer/blockentities/EntityInsideBlockRenderer.java
@@ -1,21 +1,17 @@
 package ca.fxco.gametestlib.client.renderer.blockentities;
 
-import ca.fxco.gametestlib.Utils.RenderUtils;
-import ca.fxco.gametestlib.base.GameTestBlocks;
 import ca.fxco.gametestlib.base.GameTestProperties;
 import ca.fxco.gametestlib.blocks.EntityInsideBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.debug.DebugRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import org.joml.Matrix4f;
 
 @Environment(EnvType.CLIENT)
 public class EntityInsideBlockRenderer<T extends EntityInsideBlockEntity> implements BlockEntityRenderer<T> {
@@ -26,11 +22,8 @@ public class EntityInsideBlockRenderer<T extends EntityInsideBlockEntity> implem
     public void render(T blockEntity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j) {
         Level level = blockEntity.getLevel();
         if (level != null) {
-            Matrix4f matrix4f = poseStack.last().pose();
             BlockPos blockPos = blockEntity.getBlockPos();
-            RenderUtils.renderCubeWithCull(matrix4f, multiBufferSource.getBuffer(RenderType.lightning()), dir ->
-                level.getBlockState(blockPos.relative(dir)).getBlock() != GameTestBlocks.ENTITY_INSIDE_BLOCK
-            );
+            DebugRenderer.renderFilledUnitCube(poseStack, multiBufferSource, blockPos, 0x7F, 0x7F, 0x7F, 0x7F);
             BlockState state = blockEntity.getBlockState();
             String entityType = state.getValue(GameTestProperties.ENTITY_TYPE).getSerializedName() +
                     " [" + state.getValue(GameTestProperties.DELAY_32) + "]";
