8f4e63fee08f44a29c9a18ff9d6abbbe6d656f4f
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-01-19 20:49:36 -0500

    Fix Game test runner mixin

--- src/main/java/ca/fxco/gametestlib/mixin/gametest/GameTestRunnerMixin.java
+++ src/main/java/ca/fxco/gametestlib/mixin/gametest/GameTestRunnerMixin.java
@@ -2,30 +2,42 @@ package ca.fxco.gametestlib.mixin.gametest;
 
 import ca.fxco.api.gametestlib.control.GameTestControl;
 import ca.fxco.gametestlib.GameTestLibMod;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTestInfo;
 import net.minecraft.gametest.framework.GameTestRunner;
 import net.minecraft.gametest.framework.GameTestTicker;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Collection;
+
 @Mixin(GameTestRunner.class)
 public class GameTestRunnerMixin {
 
+    @Shadow @Final private GameTestTicker testTicker;
+
     @Inject(
-            method = "runTest",
+            method = "runBatch",
             at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/gametest/framework/GameTestTicker;" +
-                            "add(Lnet/minecraft/gametest/framework/GameTestInfo;)V"
+                    value = "FIELD",
+                    target = "Lnet/minecraft/gametest/framework/GameTestRunner;" +
+                            "testTicker:Lnet/minecraft/gametest/framework/GameTestTicker;"
             )
     )
-    private static void useCustomListeners(GameTestInfo gameTestInfo, BlockPos blockPos,
-                                           GameTestTicker gameTestTicker, CallbackInfo ci) {
+    private void gtl$useCustomListeners(int i, CallbackInfo ci, @Local Collection<GameTestInfo> collection) {
         for (GameTestControl.GameTestListenerFactory create : GameTestLibMod.CONTROLLER.getGameTestListeners()) {
-            gameTestInfo.addListener(create.apply(gameTestInfo, gameTestTicker, blockPos));
+            for (GameTestInfo gameTestInfo : collection) {
+                gameTestInfo.addListener(create.apply(
+                        gameTestInfo,
+                        this.testTicker,
+                        gameTestInfo.getStructureBlockPos()
+                ));
+            }
         }
     }
 }
