f02912428910a87b1cf9afedb7b0e26a9c0410f3
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-01-19 20:34:16 -0500

    Fix block registration

--- src/main/java/ca/fxco/gametestlib/base/GameTestBlocks.java
+++ src/main/java/ca/fxco/gametestlib/base/GameTestBlocks.java
@@ -3,9 +3,14 @@ package ca.fxco.gametestlib.base;
 import ca.fxco.gametestlib.blocks.*;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+
+import java.util.function.Function;
 
 import static ca.fxco.gametestlib.GameTestLibMod.id;
 import static net.minecraft.world.level.material.PushReaction.*;
@@ -15,31 +20,40 @@ public class GameTestBlocks {
     // GameTest Blocks
     public static final PulseStateBlock PULSE_STATE_BLOCK = register(
             "pulse_state_block",
-            new PulseStateBlock(Properties.ofFullCopy(Blocks.STONE).pushReaction(BLOCK))
+            PulseStateBlock::new,
+            Properties.ofFullCopy(Blocks.STONE).pushReaction(BLOCK)
     );
     public static final CheckStateBlock CHECK_STATE_BLOCK = register(
             "check_state_block",
-            new CheckStateBlock(Properties.ofFullCopy(Blocks.STONE).pushReaction(BLOCK))
+            CheckStateBlock::new,
+            Properties.ofFullCopy(Blocks.STONE).pushReaction(BLOCK)
     );
     public static final TestTriggerBlock TEST_TRIGGER_BLOCK = register(
             "test_trigger_block",
-            new TestTriggerBlock(Properties.ofFullCopy(Blocks.WHITE_CONCRETE).pushReaction(BLOCK))
+            TestTriggerBlock::new,
+            Properties.ofFullCopy(Blocks.WHITE_CONCRETE).pushReaction(BLOCK)
     );
     public static final GameTestPoweredBlock GAMETEST_REDSTONE_BLOCK = register(
             "gametest_redstone_block",
-            new GameTestPoweredBlock(Properties.ofFullCopy(Blocks.REDSTONE_BLOCK).pushReaction(BLOCK))
+            GameTestPoweredBlock::new,
+            Properties.ofFullCopy(Blocks.REDSTONE_BLOCK).pushReaction(BLOCK)
     );
     public static final EntityInteractionBlock ENTITY_INTERACTION_BLOCK = register(
             "entity_interaction_block",
-            new EntityInteractionBlock(Properties.ofFullCopy(Blocks.STONE).pushReaction(BLOCK))
+            EntityInteractionBlock::new,
+            Properties.ofFullCopy(Blocks.STONE).pushReaction(BLOCK)
     );
     public static final EntityInsideBlock ENTITY_INSIDE_BLOCK = register(
             "entity_inside_block",
-            new EntityInsideBlock(Properties.ofFullCopy(Blocks.STONE).noCollission().pushReaction(BLOCK))
+            EntityInsideBlock::new,
+            Properties.ofFullCopy(Blocks.STONE).noCollission().pushReaction(BLOCK)
     );
 
-    private static <T extends Block> T register(String name, T block) {
-        return Registry.register(BuiltInRegistries.BLOCK, id(name), block);
+    public static <T extends Block> T register(String name, Function<Properties, T> function,
+                                               BlockBehaviour.Properties properties) {
+        ResourceKey<Block> resourceKey = ResourceKey.create(Registries.BLOCK, id(name));
+        T block = function.apply(properties.setId(resourceKey));
+        return Registry.register(BuiltInRegistries.BLOCK, resourceKey, block);
     }
 
     public static void boostrap() {}
